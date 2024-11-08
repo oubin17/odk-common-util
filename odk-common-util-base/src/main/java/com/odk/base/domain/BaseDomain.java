4786c83f784a62842cfd742650de338f25d1800c
oubin <oubin17@gmail.com>
2024-11-08 09:33:04 +0800

    Spring Boot 3.0.2版本支持

--- odk-common-util-base/src/main/java/com/odk/base/domain/BaseDomain.java
+++ odk-common-util-base/src/main/java/com/odk/base/domain/BaseDomain.java
@@ -1,5 +1,11 @@
 package com.odk.base.domain;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Date;
--- odk-common-util-base/src/main/java/com/odk/base/dos/BaseDO.java
+++ odk-common-util-base/src/main/java/com/odk/base/dos/BaseDO.java
@@ -1,7 +1,15 @@
 package com.odk.base.dos;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
  * BaseDO
@@ -10,22 +18,37 @@ import java.util.Date;
  * @version: 1.0
  * @author: oubin on 2024/1/18
  */
+@MappedSuperclass
 public class BaseDO extends DO {
-    private static final long serialVersionUID = -279848917774829594L;
 
+    private static final long serialVersionUID = -5695130590258342820L;
     /**
      * 创建时间
      */
+    @Column(name = "create_time")
+    @CreatedDate
     private LocalDateTime createTime;
+
+    @Column(name = "create_by")
+    @CreatedBy
+    private String createBy;
 
     /**
      * 更新时间
      */
+    @Column(name = "update_time")
+    @LastModifiedDate
     private LocalDateTime updateTime;
+
+
+    @Column(name = "update_by")
+    @LastModifiedBy
+    private String updateBy;
 
     /**
      * 租户ID
      */
+    @Column(name = "tenant_id")
     private String tenantId;
 
     public LocalDateTime getCreateTime() {
@@ -42,6 +65,14 @@ public class BaseDO extends DO {
 
     public void setUpdateTime(LocalDateTime updateTime) {
         this.updateTime = updateTime;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
     }
 
     public String getTenantId() {
